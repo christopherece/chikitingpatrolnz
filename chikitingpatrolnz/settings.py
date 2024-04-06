fd304c205631767c1dc94c37b3a9d782a65a4dd2
christopherece <christopheranchetaece@gmail.com>
2024-04-06 20:03:49 +1300

    Added Host

--- chikitingpatrolnz/settings.py
+++ chikitingpatrolnz/settings.py
@@ -28,9 +28,9 @@ SECRET_KEY = 'django-insecure-o2q^+$6!gt&i8p$5q%_mbv-kjv98uffxc3#b+)p1n=_0cqq@r-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '192.168.10.221','chikitingpatrolnz.com','travelblog.chikitingpatrolnz.com']
+ALLOWED_HOSTS = ['localhost', '192.168.10.221','chikitingpatrolnz.balaydalakay.com','travelblog.chikitingpatrolnz.com']
 CSRF_TRUSTED_ORIGINS = [
-    'https://chikitingpatrolnz.com','http://chikitingpatrolnz.com'
+    'https://chikitingpatrolnz.balaydalakay.com','http://chikitingpatrolnz.balaydalakay.com'
 ]
 
 
